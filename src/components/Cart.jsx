09a3ddfc84fcc1704928d23199477afc0a678289
soykot49 <mdrezwannurrashid26@gmail.com>
2024-11-17 12:12:08 +0600

    change

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom"; 
 import { getProducts, removeFromCart } from "../utils";
 import CartSingleProduct from "./CartSingleProduct";
-import groupImage from "../assets/group.png";
+import groupImage from "../assets/Group.png";
 
 const Cart = () => {
   const [cartProduct, setCartProduct] = useState([]);
